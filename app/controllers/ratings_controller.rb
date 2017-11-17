70f50ea40314d660c02d7b75dbd74bb49336ae2e
Maks Ashmetev <m.ashmetev@gmail.com>
2017-11-17 14:57:59 +0300

    Create rating.

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,25 @@
+class RatingsController < ApplicationController
+  #allowable value of rating
+  @@rating = [1,2,3,4,5]
+
+  def create
+    if data_valid?
+      @middle_rating = ((@post['middle_rating']||0) * (@post['num_of_voices']||1) + @data['rating']) / ((@post['num_of_voices']||0) + 1)
+      @post.update('num_of_voices': (@post['num_of_voices']||0) + 1, 'middle_rating': @middle_rating)
+      render json: {middle_rating: @middle_rating}
+     else
+      render status: 422
+     end
+  end
+
+  private
+    def data_valid?
+      @data = JSON.parse( params[:data] )
+      @post = Post.find_by( id: @data['id'] )
+      if @post && @@rating.include?( @data['rating'] )
+        true
+      else
+        false
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'posts/create'
+  post 'ratings/create'
 end
